a57a3d375312afbbf876d3d68d7381f484dae234
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-29 22:39:10 -0300

    Regenerate lazy wrapper exports

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,12 +3,48 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from flext_core.lazy import (
     build_lazy_import_map,
     install_lazy_exports,
     merge_lazy_imports,
 )
 
+if TYPE_CHECKING:
+    from flext_tests import (
+        d as d,
+        e as e,
+        h as h,
+        r as r,
+        td as td,
+        tf as tf,
+        tk as tk,
+        tm as tm,
+        tv as tv,
+        x as x,
+    )
+
+    from tests.base import (
+        TestsFlextTapLdifServiceBase as TestsFlextTapLdifServiceBase,
+        s as s,
+    )
+    from tests.constants import (
+        TestsFlextTapLdifConstants as TestsFlextTapLdifConstants,
+        c as c,
+    )
+    from tests.models import TestsFlextTapLdifModels as TestsFlextTapLdifModels, m as m
+    from tests.protocols import (
+        TestsFlextTapLdifProtocols as TestsFlextTapLdifProtocols,
+        p as p,
+    )
+    from tests.settings import TestsFlextTapLdifSettings as TestsFlextTapLdifSettings
+    from tests.typings import TestsFlextTapLdifTypes as TestsFlextTapLdifTypes, t as t
+    from tests.unit.test_tap import TestsFlextTapLdifTap as TestsFlextTapLdifTap
+    from tests.utilities import (
+        TestsFlextTapLdifUtilities as TestsFlextTapLdifUtilities,
+        u as u,
+    )
 _LAZY_IMPORTS = merge_lazy_imports(
     (".unit",),
     build_lazy_import_map(
