fba828573347476ee48bb544abd69ff65b811e3a
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-05 15:49:15 -0300

    [WIP] refactor: checkpoint fleet conformance

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,17 +3,16 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from types import MappingProxyType
+from typing import TYPE_CHECKING
 
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from . import unit as unit
     from flext_tap_ldif import FlextTapLdifConstants
     from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
 
+    from . import unit as unit
     from .base import TestsFlextTapLdifServiceBase, TestsFlextTapLdifServiceBase as s
     from .constants import TestsFlextTapLdifConstants, TestsFlextTapLdifConstants as c
     from .models import TestsFlextTapLdifModels, TestsFlextTapLdifModels as m
