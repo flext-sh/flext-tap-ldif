7751a7f16e8e317d759eedc335ac2bd785cb929f
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-20 16:56:35 -0300

    chore(gen): converge fleet projections (post integration landing)

--- src/flext_tap_ldif/__init__.py
+++ src/flext_tap_ldif/__init__.py
@@ -9,14 +9,8 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 from .__version__ import (
-    __author__ as __author__,
-    __author_email__ as __author_email__,
-    __description__ as __description__,
-    __license__ as __license__,
-    __title__ as __title__,
-    __url__ as __url__,
-    __version__ as __version__,
-    __version_info__ as __version_info__,
+    __author__ as __author__, __author_email__ as __author_email__, __description__ as __description__, __license__ as __license__,
+    __title__ as __title__, __url__ as __url__, __version__ as __version__, __version_info__ as __version_info__,
 )
 
 if TYPE_CHECKING:
@@ -33,40 +27,15 @@ if TYPE_CHECKING:
     from .typings import FlextTapLdifTypes, FlextTapLdifTypes as t
     from .utilities import FlextTapLdifUtilities, FlextTapLdifUtilities as u
 __all__: tuple[str, ...] = (
-    "FlextLdifConstants",
-    "FlextTapLdif",
-    "FlextTapLdifCli",
-    "FlextTapLdifConfig",
-    "FlextTapLdifConstants",
-    "FlextTapLdifModels",
-    "FlextTapLdifProtocols",
-    "FlextTapLdifService",
-    "FlextTapLdifSettings",
-    "FlextTapLdifTypes",
-    "FlextTapLdifUtilities",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "config",
-    "d",
-    "e",
-    "h",
-    "m",
-    "main",
-    "p",
-    "r",
-    "s",
-    "settings",
-    "t",
-    "tap_ldif",
-    "u",
-    "x",
+    "FlextLdifConstants", "FlextTapLdif", "FlextTapLdifCli", "FlextTapLdifConfig",
+    "FlextTapLdifConstants", "FlextTapLdifModels", "FlextTapLdifProtocols", "FlextTapLdifService",
+    "FlextTapLdifSettings", "FlextTapLdifTypes", "FlextTapLdifUtilities", "__author__",
+    "__author_email__", "__description__", "__license__", "__title__",
+    "__url__", "__version__", "__version_info__", "c",
+    "config", "d", "e", "h",
+    "m", "main", "p", "r",
+    "s", "settings", "t", "tap_ldif",
+    "u", "x",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
