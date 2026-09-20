65e75844e3d1d9878946cfb6dc8d31bef3b6e419
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-20 18:05:02 -0300

    fixes

--- src/flext_tap_ldif/_utilities/__init__.py
+++ src/flext_tap_ldif/_utilities/__init__.py
@@ -14,7 +14,10 @@ if TYPE_CHECKING:
     from .processor import FlextTapLdifUtilitiesProcessor
     from .state_management import FlextTapLdifUtilitiesStateManagement
 __all__: tuple[str, ...] = (
-    "FlextTapLdifUtilitiesEntriesStream", "FlextTapLdifUtilitiesLdifDataProcessing", "FlextTapLdifUtilitiesProcessor", "FlextTapLdifUtilitiesStateManagement",
+    "FlextTapLdifUtilitiesEntriesStream",
+    "FlextTapLdifUtilitiesLdifDataProcessing",
+    "FlextTapLdifUtilitiesProcessor",
+    "FlextTapLdifUtilitiesStateManagement",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
