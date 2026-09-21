cc2cfbb9b883ef5895662302909d64892e36e689
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 12:21:23 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- src/flext_tap_ldif/__init__.py
+++ src/flext_tap_ldif/__init__.py
@@ -20,8 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_core import d, e, h, r, x
     from flext_ldif import s
+
+    from flext_core import d, e, h, r, x
 
     from .__version__ import FlextTapLdifVersion
     from ._config import FlextTapLdifConfig, config
