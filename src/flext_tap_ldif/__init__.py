8d9341c6fbb93aa043573e9f1bd017f5b05bd093
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 17:54:59 -0300

    chore(docs): land docs-lifecycle output (generate/fix/fmt)

--- src/flext_tap_ldif/__init__.py
+++ src/flext_tap_ldif/__init__.py
@@ -20,24 +20,28 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import cli
-    from flext_ldif import ldif
-    from flext_meltano import meltano, s
-    from pydantic_core import from_json, to_json, to_jsonable_python
+    from flext_ldif import (
+        cli,
+        core,
+        from_json,
+        lazy_attribute,
+        ldif,
+        servers,
+        to_json,
+        to_jsonable_python,
+    )
+    from flext_meltano import d, e, h, meltano, r, s, services, x
 
-    from flext_core import core, d, e, h, lazy_attribute, r, x
-
-    from .__version__ import FlextTapLdifVersion
     from ._config import FlextTapLdifConfig, config
     from ._settings import FlextTapLdifSettings, settings
     from .api import FlextTapLdifService, tap_ldif
     from .cli import FlextTapLdifCli, main
-    from .constants import FlextTapLdifConstants, FlextTapLdifConstants as c
-    from .models import FlextTapLdifModels, FlextTapLdifModels as m
-    from .protocols import FlextTapLdifProtocols, FlextTapLdifProtocols as p
+    from .constants import FlextTapLdifConstants, c
+    from .models import FlextTapLdifModels, m
+    from .protocols import FlextTapLdifProtocols, p
     from .tap import FlextTapLdif
-    from .typings import FlextTapLdifTypes, FlextTapLdifTypes as t
-    from .utilities import FlextTapLdifUtilities, FlextTapLdifUtilities as u
+    from .typings import FlextTapLdifTypes, t
+    from .utilities import FlextTapLdifUtilities, u
 __all__: tuple[str, ...] = (
     "FlextTapLdif",
     "FlextTapLdifCli",
@@ -49,7 +53,6 @@ __all__: tuple[str, ...] = (
     "FlextTapLdifSettings",
     "FlextTapLdifTypes",
     "FlextTapLdifUtilities",
-    "FlextTapLdifVersion",
     "__author__",
     "__author_email__",
     "__description__",
@@ -59,18 +62,28 @@ __all__: tuple[str, ...] = (
     "__version__",
     "__version_info__",
     "c",
+    "cli",
     "config",
+    "core",
     "d",
     "e",
+    "from_json",
     "h",
+    "lazy_attribute",
+    "ldif",
     "m",
     "main",
+    "meltano",
     "p",
     "r",
     "s",
+    "servers",
+    "services",
     "settings",
     "t",
     "tap_ldif",
+    "to_json",
+    "to_jsonable_python",
     "u",
     "x",
 )
@@ -78,7 +91,6 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".__version__": ("FlextTapLdifVersion",),
             "._config": ("FlextTapLdifConfig", "config"),
             "._settings": ("FlextTapLdifSettings", "settings"),
             ".api": ("FlextTapLdifService", "tap_ldif"),
@@ -89,11 +101,17 @@ _LAZY_IMPORTS = MappingProxyType(
             ".tap": ("FlextTapLdif",),
             ".typings": ("FlextTapLdifTypes", "t"),
             ".utilities": ("FlextTapLdifUtilities", "u"),
-            "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
-            "flext_ldif": ("ldif",),
-            "flext_meltano": ("meltano", "s"),
-            "pydantic_core": ("from_json", "to_json", "to_jsonable_python"),
+            "flext_ldif": (
+                "cli",
+                "core",
+                "from_json",
+                "lazy_attribute",
+                "ldif",
+                "servers",
+                "to_json",
+                "to_jsonable_python",
+            ),
+            "flext_meltano": ("d", "e", "h", "meltano", "r", "s", "services", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
